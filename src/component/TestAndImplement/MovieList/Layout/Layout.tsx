202add822cc0f173a78eb6d2905a956c57f6f6a5
mkrahulbiswas <biswas.rahul31@gmail.com>
2025-01-31 15:18:56 +0530

    jj

--- src/component/TestAndImplement/MovieList/Layout/Layout.tsx
+++ src/component/TestAndImplement/MovieList/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Footer from "../Includes/Footer";
 import Header from "../Includes/Header";
 import { useContext, useState } from "react";
@@ -6,14 +6,27 @@ import { LoaderContext } from "../Context/LoaderContext";
 
 export const Layout = () => {
   const [newLoader, setNewLoader] = useState(0)
+  const navigation = useNavigation()
+  console.log(navigation.state);
+
   return (
-    <div className="mainSection">
-      <Loader />
-      <Header />
-      {/* <p>newLoader:{newLoader}</p> */}
-      <Outlet context={{ newLoader, setNewLoader }} />
-      <Footer />
-    </div>
+    <>
+      {
+        (navigation.state === 'loading') ?
+          <div className="mainSection">
+            <div className="loader">
+              <span>Loading....</span>
+            </div>
+          </div>
+          : <div className="mainSection">
+            <Loader />
+            <Header />
+            {/* <p>newLoader:{newLoader}</p> */}
+            <Outlet context={{ newLoader, setNewLoader }} />
+            <Footer />
+          </div>
+      }
+    </>
   )
 }
 

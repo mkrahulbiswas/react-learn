8a264a2ad92b4e2c70df3ee80ee532a1452bd9d0
mkrahulbiswas <biswas.rahul31@gmail.com>
2025-02-05 16:02:20 +0530

    Done 05-02-2025

--- src/component/ReactLearnTwo/HandledApi/HandledApiApp.tsx
+++ src/component/ReactLearnTwo/HandledApi/HandledApiApp.tsx
@@ -1,11 +1,13 @@
 import DeleteApi from "./DeleteApi/DeleteApi"
 import GetApi from "./GetApi/GetApi"
+import PatchApi from "./PatchApi/PatchApi"
 import PostApi from "./PostApi/PostApi"
 import PutApi from "./PutApi/PutApi"
 
 function HandledApiApp() {
   return (
     <>
+      <PatchApi />
       <PutApi />
       <DeleteApi />
       <PostApi />

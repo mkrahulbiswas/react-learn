3542a30d4d3e93c9c097899b04e0ede9bebe0bef
mkrahulbiswas <biswas.rahul31@gmail.com>
2025-03-01 16:06:49 +0530

    asa

--- src/component/TestAndImplement/PracticeCrud/WithTemplatingCrud/WithBasicFeature/WithRouterTraditional/Includes/Footer.tsx
+++ src/component/TestAndImplement/PracticeCrud/WithTemplatingCrud/WithBasicFeature/WithRouterTraditional/Includes/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mainFooter">
       <div className="subFooter">
-        div.
+        <div className="logo"></div>
       </div>
     </footer>
   )

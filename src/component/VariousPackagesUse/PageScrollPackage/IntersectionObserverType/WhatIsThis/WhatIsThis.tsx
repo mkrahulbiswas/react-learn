9feb0b4bc17e0c578369e39edb54efaf207a11b2
mkrahulbiswas <biswas.rahul31@gmail.com>
2025-03-08 17:55:28 +0530

    aa

--- src/component/VariousPackagesUse/PageScrollPackage/IntersectionObserverType/WhatIsThis/WhatIsThis.tsx
+++ src/component/VariousPackagesUse/PageScrollPackage/IntersectionObserverType/WhatIsThis/WhatIsThis.tsx
@@ -1,10 +1,10 @@
 export const WhatIsThis = () => {
   return (
     < >
-      <span className="singleLineText">This is <b>react icon</b> package to manage the <b>icons</b></span>
-      <span className="singleLineText">To use this package you need to <b>install</b> the library by using command <b>npm install react-icons --save</b></span>
-      <span className="singleLineText">For more info you can visit the site <b>https://react-icons.github.io/react-icons/</b></span>
-      <span className="singleLineText">Some example of <b>react icon</b> is:</span>
+      <span className="singleLineText">This is <b>intersection observer</b> package to manage the <b>page scroll event</b></span>
+      <span className="singleLineText">To use this package you need to <b>install</b> the library by using command <b>npm install react-intersection-observer --save</b></span>
+      <span className="singleLineText">For more info you can visit the site <b>https://github.com/thebuilder/react-intersection-observer#readme</b> or <b>https://www.npmjs.com/package/react-intersection-observer</b></span>
+      <span className="singleLineText">Some example of <b>intersection observer</b> is:</span>
     </>
   )
 }
--- src/component/VariousPackagesUse/VariousPackagesUse.tsx
+++ src/component/VariousPackagesUse/VariousPackagesUse.tsx
@@ -4,14 +4,15 @@ import { IconPackage } from './IconPackage/IconPackage'
 import { LoaderPackage } from './LoaderPackage/LoaderPackage'
 import { PaginationPackage } from './PaginationPackage/PaginationPackage'
 import { TostAlertPackage } from './TostAlertPackage/TostAlertPackage'
+import { PageScrollPackage } from './PageScrollPackage/PageScrollPackage'
 
 export const VariousPackagesUse = () => {
   return (
     <div className="variousPackagesUse">
+      <PageScrollPackage />
       <TostAlertPackage />
       <DateTimePackage />
       {/* <UiComponentPackage /> */}
-      {/* <PageScrollPackage /> */}
 
 
       <PaginationPackage />

38b52d058579acde2107f04d34cca4966e5d6a65
mkrahulbiswas <biswas.rahul31@gmail.com>
2025-03-10 11:39:32 +0530

    a

--- src/component/VariousPackagesUse/StylePackage/StyledComponentsType/HowToUse/ExampleOne/ExampleOne.tsx
+++ src/component/VariousPackagesUse/StylePackage/StyledComponentsType/HowToUse/ExampleOne/ExampleOne.tsx
@@ -1,12 +1,68 @@
+import styled from "styled-components";
+
 export const ExampleOne = () => {
   return (
     <>
-      <div className="exampleCommon fullWidth">
-        <div className="exampleCommonPoints">
-          <span>Making <b>different position</b> of pagination</span>
-        </div>
-        <div className="exampleCommonContent">
+      <WrapperInsideTitle />
+      <ButtonAsStyledProps />
+    </>
+  )
+}
 
+export const WrapperInsideTitle = () => {
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #BF4F74;
+  `;
+
+  const Wrapper = styled.section`
+    padding: 5px;
+    background: #d5ffff;
+  `;
+
+  return (
+    <>
+      <div className="exampleCommon">
+        {/* <div className="exampleCommonPoints">
+          <span>Making <b>different position</b> of pagination</span>
+        </div> */}
+        <div className="exampleCommonContent">
+          <Wrapper>
+            <Title>
+              Hello World!
+            </Title>
+          </Wrapper>
+        </div>
+      </div >
+    </>
+  )
+}
+
+export const ButtonAsStyledProps = () => {
+  const Button = styled.button`
+    color: #BF4F74;
+    font-size: 1em;
+    margin-right: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #BF4F74;
+    border-radius: 3px;
+  `;
+
+  const TomatoButton = styled(Button)`
+    color: tomato;
+    border-color: tomato;
+    margin-right: 0em;
+  `;
+
+  return (
+    <>
+      <div className="exampleCommon">
+        <div className="exampleCommonContent exampleCommonContentFloat">
+          <Button>Normal Button</Button>
+        </div>
+        <div className="exampleCommonContent exampleCommonContentFloat">
+          <TomatoButton>Tomato Button</TomatoButton>
         </div>
       </div >
     </>
--- src/component/VariousPackagesUse/StylePackage/StyledComponentsType/WhatIsThis/WhatIsThis.tsx
+++ src/component/VariousPackagesUse/StylePackage/StyledComponentsType/WhatIsThis/WhatIsThis.tsx
@@ -1,10 +1,10 @@
 export const WhatIsThis = () => {
   return (
     < >
-      <span className="singleLineText">This is <b>react paginate</b> package to manage the <b>pagination</b></span>
-      <span className="singleLineText">To use this package you need to <b>install</b> the library by using command <b>npm install react-loader-spinner --save</b></span>
-      <span className="singleLineText">For more info you can visit the site <b>https://github.com/AdeleD/react-paginate</b> and <b>https://github.com/AdeleD/react-paginate/blob/master/demo/js/demo.js</b></span>
-      <span className="singleLineText">Some example of <b>react paginate</b> is:</span>
+      <span className="singleLineText">This is <b>styled component</b> package to manage the <b>css</b></span>
+      <span className="singleLineText">To use this package you need to <b>install</b> the library by using command <b>npm install styled-components</b></span>
+      <span className="singleLineText">For more info you can visit the site <b>https://github.com/styled-components/styled-components</b> or <b>https://styled-components.com/</b></span>
+      <span className="singleLineText">Some example of <b>styled component</b> is:</span>
     </>
   )
 }
